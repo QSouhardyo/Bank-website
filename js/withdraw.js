661dd810763aedee314550ff4810cd3f6573a130
QSouhardyo <quaziaumio98@gmail.com>
2022-08-20 17:20:20 +0600

    finish

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,22 +1,36 @@
 document.getElementById('btn-withdraw').addEventListener('click', function () {
     const withdrawField = document.getElementById('withdraw-field');
     const currentWitdrawAmountString = withdrawField.value;
-    const currentWitdrawAmount = parseFloat(currentWitdrawAmountString)
-    console.log(currentWitdrawAmountString)
+    const currentWitdrawAmount = parseFloat(currentWitdrawAmountString);
+    withdrawField.value = '';
+
+    if (isNaN(currentWitdrawAmount)) {
+        alert('please provid valid number')
+        return;
+    }
 
     const previosWithdrawTotal = document.getElementById('withdraw-total')
     const previosWithdrawTotalString = previosWithdrawTotal.innerText;
     const previosWithdrawTotalAmount = parseFloat(previosWithdrawTotalString)
 
-    const withdrawTotalCart = currentWitdrawAmount + previosWithdrawTotalAmount;
+    // const withdrawTotalCart = currentWitdrawAmount + previosWithdrawTotalAmount;
 
-    previosWithdrawTotal.innerText = withdrawTotalCart
+    // previosWithdrawTotal.innerText = withdrawTotalCart
 
     const totalDeposit = document.getElementById('total-deposit');
     const totalDepositString = totalDeposit.innerText;
     const totalDepositBalance = parseFloat(totalDepositString)
+
+    if (currentWitdrawAmount > totalDepositBalance) {
+        alert('You dont have sufficient balance ')
+        return;
+    }
+
+    const withdrawTotalCart = currentWitdrawAmount + previosWithdrawTotalAmount;
+    previosWithdrawTotal.innerText = withdrawTotalCart
+
     const newBalance = totalDepositBalance - currentWitdrawAmount;
     totalDeposit.innerText = newBalance;
 
-    withdrawField.value = '';
+
 })
